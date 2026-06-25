ac9bc392368c1a1e8b27150a6f59c036365bd357
GuiMusa <g.musaraganyi@gmail.com>
2026-06-25 11:57:07 -0400

    migration

---
+++ database/migrations/2026_06_25_130937_create_article_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('article', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->string('slug',255)->unique; /* etiquette */
+            $table->longtext('content');
+            $table->enum('status',['draft','published'])->default('draft')->nullable;
+            $table->timestamp('published_at')->nullable;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article');
+    }
+};
