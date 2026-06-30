f7ee6c068bcd19ed25958ff7436a793156023ed7
GuiMusa <g.musaraganyi@gmail.com>
2026-06-30 07:57:20 -0400

    vue_route

--- app/Http/Controllers/articleController.php
+++ app/Http/Controllers/articleController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class articleController extends Controller
 {
-    //
+    //methode show
+    public function show(): View {
+       return view('articles-list');
+    }
 }
