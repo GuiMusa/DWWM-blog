3555fef5502d305b5cfabd03705f4cadd4f319dd
GuiMusa <g.musaraganyi@gmail.com>
2026-07-22 12:20:04 -0400

    feat: update user factory and migration; change name to firstname and lastname, add role field

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -3,5 +3,5 @@
     <p>{{$article->content}}</p>
     <p>Par {{$article->user->name}}</p>
     <p>créer {{$article->created_at}}</p>
-    <p>Catégories : {{$category->category->name}}</p>
+    <p>Catégories : {{$category->category->name ?? 'Sans catégorie'}}</p>
 </div>
